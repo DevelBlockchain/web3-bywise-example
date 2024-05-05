77215bb743eaa300f93bcfda329520d57aae9f36
felipemarts <felipebrasil93@gmail.com>
2024-05-05 14:58:17 -0300

    Updates

---
+++ _next/static/ZkSft_xsZsyKnNAB-CmUg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/674-f6df1b93718638ec.js","static/css/40e1bcc55798592e.css","static/chunks/pages/index-4f6b35729b36df0f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
